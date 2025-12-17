03842f4c3e7be997be317e1bf42d91accb835aa7
gbolo <george.bolo@gmail.com>
2025-12-17 18:14:26 -0500

    remove acls when users are disabled or deleted

--- dataprovider/ddns.go
+++ dataprovider/ddns.go
@@ -60,56 +60,113 @@ func (p *DdnsProvider) SyncUser(userID string) error {
 	}
 
 	p.lock.Lock()
-
-	// Check if this is a new user or if DNS names changed
 	oldUser, existed := p.users[userID]
-	hasChanges := false
+	p.lock.Unlock()
 
-	if !existed {
-		hasChanges = len(user.DNSNames) > 0
-		log.Debugf("new user %s with %d DNS names", userID, len(user.DNSNames))
-	} else {
-		// Check if DNS names changed
-		hasChanges = !stringSlicesEqual(oldUser.DNSNames, user.DNSNames) ||
-			oldUser.ACLAllowAll != user.ACLAllowAll ||
-			!stringSlicesEqual(oldUser.ACLAllowedHosts, user.ACLAllowedHosts)
+	// Determine what changed
+	hasChanges, wasDisabled := detectUserChanges(user, &oldUser, existed)
 
-		if hasChanges {
-			log.Infof("user %s DNS config changed, resyncing", userID)
-
-			// Find DNS names that were removed
-			removedDNSNames := findRemovedStrings(oldUser.DNSNames, user.DNSNames)
-
-			// Remove old FQDNs from tracking
-			for _, fqdn := range oldUser.DNSNames {
-				delete(p.fqdns, fqdn)
-			}
-
-			// Unlock before calling cleanupDNSACLs to avoid deadlock
-			p.lock.Unlock()
-
-			// Clean up ACLs for removed DNS names
-			if len(removedDNSNames) > 0 {
-				log.Infof("user %s removed %d DNS names, cleaning up ACLs", userID, len(removedDNSNames))
-				p.cleanupDNSACLs(userID, removedDNSNames)
-			}
-
-			// Re-acquire lock
-			p.lock.Lock()
-		}
+	// Handle DNS changes (must be done before disable to get old DNS names)
+	if hasChanges && existed {
+		p.handleUserDNSChanges(userID, user, &oldUser)
 	}
 
-	// Update user cache
+	// Handle user disable
+	if wasDisabled {
+		p.handleUserDisable(userID, &oldUser)
+	}
+
+	// Update user cache and FQDNs
+	p.updateUserCache(userID, user)
+
+	// Trigger ACL resolution if changes occurred and user is enabled
+	if hasChanges && user.Enabled {
+		p.updateACLs()
+	}
+
+	return nil
+}
+
+// detectUserChanges determines if a user has changes or was disabled
+func detectUserChanges(user, oldUser *User, existed bool) (hasChanges, wasDisabled bool) {
+	if !existed {
+		hasChanges = len(user.DNSNames) > 0 && user.Enabled
+		log.Debugf("new user %s with %d DNS names", user.ID, len(user.DNSNames))
+		return
+	}
+
+	// Check if user was disabled
+	if oldUser.Enabled && !user.Enabled {
+		wasDisabled = true
+		log.Infof("user %s was disabled, cleaning up DDNS ACLs", user.ID)
+	}
+
+	// Check if DNS names or ACL config changed
+	hasChanges = !stringSlicesEqual(oldUser.DNSNames, user.DNSNames) ||
+		oldUser.ACLAllowAll != user.ACLAllowAll ||
+		!stringSlicesEqual(oldUser.ACLAllowedHosts, user.ACLAllowedHosts) ||
+		(!oldUser.Enabled && user.Enabled) // Re-enabled
+
+	if hasChanges {
+		log.Infof("user %s DNS config changed, resyncing", user.ID)
+	}
+
+	return
+}
+
+// handleUserDNSChanges handles removal of DNS names and cleanup of associated ACLs
+func (p *DdnsProvider) handleUserDNSChanges(userID string, user, oldUser *User) {
+	// Find DNS names that were removed
+	removedDNSNames := findRemovedStrings(oldUser.DNSNames, user.DNSNames)
+
+	p.lock.Lock()
+	// Remove old FQDNs from tracking
+	for _, fqdn := range oldUser.DNSNames {
+		delete(p.fqdns, fqdn)
+	}
+	p.lock.Unlock()
+
+	// Clean up ACLs for removed DNS names
+	if len(removedDNSNames) > 0 {
+		log.Infof("user %s removed %d DNS names, cleaning up ACLs", userID, len(removedDNSNames))
+		p.cleanupDNSACLs(userID, removedDNSNames)
+	}
+}
+
+// handleUserDisable removes all ACLs for a disabled user
+func (p *DdnsProvider) handleUserDisable(userID string, oldUser *User) {
+	p.lock.Lock()
+	// Remove FQDNs from tracking since user is disabled
+	for _, fqdn := range oldUser.DNSNames {
+		delete(p.fqdns, fqdn)
+	}
+	p.lock.Unlock()
+
+	// Clean up ALL ACLs (DDNS and challenge-based) for this disabled user
+	log.Infof("user %s disabled, cleaning up all ACLs", userID)
+	if err := p.CleanupAllUserACLs(userID); err != nil {
+		log.Errorf("failed to cleanup ACLs for disabled user %s: %v", userID, err)
+	}
+}
+
+// updateUserCache updates the user cache and FQDN mappings
+func (p *DdnsProvider) updateUserCache(userID string, user *User) {
+	p.lock.Lock()
+	defer p.lock.Unlock()
+
+	// Update user cache (keep disabled users in cache to detect re-enable)
 	if len(user.DNSNames) > 0 {
 		p.users[userID] = *user
 
-		// Add new FQDNs
-		for _, fqdn := range user.DNSNames {
-			if validate.IsDNSName(fqdn) {
-				p.fqdns[fqdn] = ACL{
-					AllowAll:     user.ACLAllowAll,
-					AllowedHosts: user.ACLAllowedHosts,
-					UserIDs:      []string{user.ID},
+		// Only add FQDNs if user is enabled
+		if user.Enabled {
+			for _, fqdn := range user.DNSNames {
+				if validate.IsDNSName(fqdn) {
+					p.fqdns[fqdn] = ACL{
+						AllowAll:     user.ACLAllowAll,
+						AllowedHosts: user.ACLAllowedHosts,
+						UserIDs:      []string{user.ID},
+					}
 				}
 			}
 		}
@@ -117,15 +174,6 @@ func (p *DdnsProvider) SyncUser(userID string) error {
 		// User has no DNS names, remove from cache
 		delete(p.users, userID)
 	}
-
-	p.lock.Unlock()
-
-	// Only update ACLs if something changed
-	if hasChanges {
-		p.updateACLs()
-	}
-
-	return nil
 }
 
 // RemoveUser notifies the DDNS provider that a user was deleted
@@ -139,10 +187,6 @@ func (p *DdnsProvider) RemoveUser(userID string) {
 
 	log.Infof("removing user %s from DDNS provider", userID)
 
-	// Get the DNS names before removing the user
-	dnsNames := make([]string, len(user.DNSNames))
-	copy(dnsNames, user.DNSNames)
-
 	// Remove all FQDNs for this user
 	for _, fqdn := range user.DNSNames {
 		delete(p.fqdns, fqdn)
@@ -151,8 +195,11 @@ func (p *DdnsProvider) RemoveUser(userID string) {
 	delete(p.users, userID)
 	p.lock.Unlock()
 
-	// Clean up ACLs for the removed DNS names
-	p.cleanupDNSACLs(userID, dnsNames)
+	// Clean up ALL ACLs (DDNS and challenge-based) for this deleted user
+	log.Infof("user %s deleted, cleaning up all ACLs", userID)
+	if err := p.CleanupAllUserACLs(userID); err != nil {
+		log.Errorf("failed to cleanup ACLs for deleted user %s: %v", userID, err)
+	}
 
 	// Trigger ACL update
 	go p.updateACLs()
@@ -234,6 +281,57 @@ func (p *DdnsProvider) cleanupDNSACLs(userID string, dnsNames []string) {
 	}
 }
 
+// CleanupAllUserACLs removes or updates ALL ACLs (DDNS and challenge-based) for a user
+// This should be called when a user is disabled or deleted
+func (p *DdnsProvider) CleanupAllUserACLs(userID string) error {
+	if p.dataProvider == nil {
+		return nil
+	}
+
+	// Get all ACLs from data provider
+	allACLs, err := p.dataProvider.GetAllACLs()
+	if err != nil {
+		log.Errorf("failed to get ACLs during user %s cleanup: %v", userID, err)
+		return err
+	}
+
+	removedCount := 0
+	updatedCount := 0
+
+	for ip, acl := range allACLs {
+		// Check if this ACL contains the user
+		if !acl.CheckUserId(userID) {
+			continue
+		}
+
+		// Check if this user is the only one in the ACL
+		if len(acl.UserIDs) == 1 && acl.UserIDs[0] == userID {
+			// This user is the only one, remove the entire ACL
+			if err := p.dataProvider.RemoveIp(ip); err != nil {
+				log.Errorf("failed to remove ACL for IP %s: %v", ip, err)
+			} else {
+				log.Infof("removed ACL for IP %s (was only used by user %s)", ip, userID)
+				removedCount++
+			}
+		} else {
+			// Multiple users, just remove this user from the ACL
+			acl.RemoveUserId(userID)
+			if err := p.dataProvider.UpdateACL(ip, acl); err != nil {
+				log.Errorf("failed to update ACL for IP %s: %v", ip, err)
+			} else {
+				log.Debugf("removed user %s from ACL for IP %s (other users still using it)", userID, ip)
+				updatedCount++
+			}
+		}
+	}
+
+	if removedCount > 0 || updatedCount > 0 {
+		log.Infof("cleaned up ACLs for user %s: %d removed, %d updated", userID, removedCount, updatedCount)
+	}
+
+	return nil
+}
+
 // syncUsers rebuilds the FQDN map from a list of users
 func (p *DdnsProvider) syncUsers(users []User) error {
 	p.lock.Lock()
@@ -246,13 +344,15 @@ func (p *DdnsProvider) syncUsers(users []User) error {
 	usersWithDNS := 0
 	totalFQDNs := 0
 
-	for _, user := range users {
+	// Use indexing to avoid copying large structs
+	for i := range users {
+		user := &users[i]
 		if len(user.DNSNames) == 0 {
 			continue
 		}
 
 		usersWithDNS++
-		p.users[user.ID] = user
+		p.users[user.ID] = *user
 
 		for _, fqdn := range user.DNSNames {
 			if validate.IsDNSName(fqdn) {
@@ -291,16 +391,16 @@ func stringSlicesEqual(a, b []string) bool {
 	return true
 }
 
-// Helper function to find strings in 'old' that are not in 'new'
-func findRemovedStrings(old, new []string) []string {
-	newSet := make(map[string]bool)
-	for _, s := range new {
-		newSet[s] = true
+// Helper function to find strings in 'old' that are not in 'updated'
+func findRemovedStrings(old, updated []string) []string {
+	updatedSet := make(map[string]bool)
+	for _, s := range updated {
+		updatedSet[s] = true
 	}
 
 	removed := []string{}
 	for _, s := range old {
-		if !newSet[s] {
+		if !updatedSet[s] {
 			removed = append(removed, s)
 		}
 	}
@@ -396,7 +496,6 @@ func (p *DdnsProvider) daemonize() {
 		select {
 		case <-t:
 			p.updateACLs()
-			log.Debugf("periodic update for DNS based ACLs completed")
 		case <-p.stopSignal:
 			log.Warning("stop signal received, DNS based ACLs will stop being updated.")
 			return
